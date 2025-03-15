52ab6b50cce7d9dede76d2a7a66c1a6970ea3805
Kariseks <gasiorkarolpl@gmail.com>
2025-03-15 15:04:30 +0100

    add CCatch2 test to project structure

--- libs/parser/parser.cpp
+++
@@ -1,9 +0,0 @@
-#parser
-set(SOURCES ${SOURCES}
-    PARENT_SCOPE)
-
-#==================================================================================================
-add_subdirectory("./tests/")
-set(TEST_SOURCES ${SOURCES} PARENT_SCOPE)
-
-
